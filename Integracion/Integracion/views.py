7e581b35bb3035039f7ee8d29ffc8008a1c47351
Yanina <ybaeza2014@alu.uct.cl>
2020-10-14 07:24:02 -0300

    grafico implementado
    en moda.html

--- Integracion/Integracion/views.py
+++ Integracion/Integracion/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.shortcuts import render
+from random import sample
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+import numpy as np
+import io, urllib, base64
 
 def index(request):
     return render(request, "Integracion/index.html")
@@ -9,8 +14,30 @@ def tutorial(request):
     return render(request, "Integracion/tutorial.html")
 
 def moda(request):
+    # Creamos los datos para representar en el gráfico
+    ##plt.plot(range(11))
+    ##f = plt.gcf()
 
-   return render(request, "Integracion/moda.html")
+    x = range(1,11)
+    y = sample(range(20), len(x))
+
+    # Creamos una figura y le dibujamos el gráfico
+    f = plt.figure()
+
+    # Creamos los ejes
+    axes = f.add_axes([0.15, 0.15, 0.75, 0.75]) # [left, bottom, width, height]
+    axes.plot(x, y)
+    axes.set_xlabel("Eje X")
+    axes.set_ylabel("Eje Y")
+    
+    # Como enviaremos la imagen en bytes la guardaremos en un buffer
+    buf = io.BytesIO()
+    f.savefig(buf, format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    return render(request, "Integracion/moda.html", {'data': uri})
+   # return render(request, "Integracion/moda.html")
 
 def promedio(request):
 
@@ -23,3 +50,29 @@ def regresionlineal(request):
 def faq(request):
     return render(request, "Integracion/faq.html")
 
+def plot(request):
+    # Creamos los datos para representar en el gráfico
+    x = range(1,11)
+    f = plt.gcf()
+    # Como enviaremos la imagen en bytes la guardaremos en un buffer
+    buf = io.BytesIO()
+    f.savefig(buf, format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    return render(request, "Integracion/moda.html", {'data': uri})
+
+    # canvas = FigureCanvasAgg(f)
+    # canvas.print_png(buf)
+
+    # # Creamos la respuesta enviando los bytes en tipo imagen png
+    # response = HttpResponse(buf.getvalue(), content_type='image/png')
+
+    # # Limpiamos la figura para liberar memoria
+    # f.clear()
+
+    # # Añadimos la cabecera de longitud de fichero para más estabilidad
+    # response['Content-Length'] = str(len(response.content))
+
+    # # Devolvemos la response
+    # return response
